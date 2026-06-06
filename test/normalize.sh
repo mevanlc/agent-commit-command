7b4bb94910caf27b05dcf4ef9b3977193e1b6aad
Mike Clark <mevanlc@gmail.com>
2026-06-05 20:18:09 -0600

    Make test normalization agnostic to temp directory
    - Generalize commit-tool temp-path normalization for staged-backup and diff files

--- test/normalize.sh
+++ test/normalize.sh
@@ -5,6 +5,6 @@
 
 sed -E \
   -e 's/[0-9a-f]{7,40}/HASH/g' \
-  -e 's|/tmp/commit-tool-staged-backup-[0-9]+\.patch|/tmp/commit-tool-staged-backup-PID.patch|g' \
-  -e 's|/tmp/commit-tool-diff-[0-9]+\.txt|/tmp/commit-tool-diff-PID.txt|g' \
+  -e 's|/.*/commit-tool-staged-backup[^`\"[:space:]]*|/tmp/commit-tool-staged-backup-PID.patch|g' \
+  -e 's|/.*/commit-tool-diff[^`\"[:space:]]*|/tmp/commit-tool-diff-PID.txt|g' \
   -e 's/\*\*Diff too large for inline display\*\* \([0-9]+ characters\)/**Diff too large for inline display** (NNNNN characters)/'
